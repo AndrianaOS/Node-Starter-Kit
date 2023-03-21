17f6a19a78e6c597f89b7db0a8e475d08235e209
Andriana <andrianasaffo@gmail.com>
2023-03-21 10:53:43 +0000

    Added comments for handler functions

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
+// HANDLER FUNCTION FOR SEARCH WORDS
 app.get("/", function (request, response) {
   const searchQuery = request.query.search;
   response.send(
@@ -9,16 +10,19 @@ app.get("/", function (request, response) {
   );
 });
 
+// HANDLER FUNCTION FOR MULTIPLYING VALUES
 app.get("/multiply", function (req, res) {
   let value1 = req.query.value1;
   let value2 = req.query.value2;
   res.send(`We are using node.js and multiplying these: ${value1 * value2}`);
 });
 
+// HANDLER FUNCTION TO CONSOLE LOG STRING AS RESPONSE TO REQUEST
 app.get("/codeyourfuture", function (req, res) {
   res.send("I'm a CYF trainee learning full stack web development");
 });
 
+// HANDLER FUNCTION TO GET AMOUNT BASED ON REQUEST
 app.get("/chocolate", function (req, res) {
   const amount = req.query.amount;
   res.send(`Send me ${amount} chocolates`);
